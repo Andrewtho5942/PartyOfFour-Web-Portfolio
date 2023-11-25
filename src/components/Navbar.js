9eb1268e573eb73d52d6a9bac38429076bdc77c9
thomp907 <andrewtho5942@gmail.com>
2023-11-24 18:53:33 -0600

    Updated navbar divisions

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,13 +26,13 @@ export default function Navbar({darkMode, changeTheme}) {
         <header className="dark:bg-black bg-white backdrop-filter dark:backdrop-blur-md backdrop-blur-md dark:bg-opacity-40  bg-opacity-40 md:sticky top-0 z-10 ">
           
           <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-            <a className="title-font font-medium mb-4 md:mb-0">
+            <p className="title-font font-medium mb-4 md:mb-0">
               
               <Link to="/" className="ml-3 text-xl font-semibold text-highlight hover:text-highlight-dark">
                 CGT270 - Party of Four 
               </Link>
               
-            </a>
+            </p>
             <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 dark:text-white text-black md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
               <Link to="/" className="mr-5 hover:text-highlight">
                 Home
@@ -49,12 +49,12 @@ export default function Navbar({darkMode, changeTheme}) {
             </nav>
 
             <ChangeThemeButton className="text-3xl" />
-            <a
+            <p
               className="inline-flex items-center dark:text-white text-black dark:bg-gray-700 bg-gray-300 opacity-70 border-0 py-1 px-3 focus:outline-none rounded-3xl text-base mt-4 ml-10 md:mt-0">
               <ChevronLeftIcon className="w-4 h-4 mr-1 mt-1" />
               Made By Andrew Thompson
               <ChevronRightIcon className="w-4 h-4 ml-1 mt-1" />
-            </a>
+            </p>
           </div>
         </header>
     );
