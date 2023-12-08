9d03ab0ebfd78a152870434b3ce7a0fd570086bb
thomp907 <andrewtho5942@gmail.com>
2023-12-08 16:37:08 -0500

    update visualizations, make gallery pictures grow on hover

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -24,103 +24,103 @@ export default function Gallery(prop) {
                     className="my-4 rounded-xl outline-main w-[64rem]"
                     src="https://onedrive.live.com/embed?resid=35F145B59F2BEC0E%21161312&authkey=%21ACvmc3Kr4OzJM2I&em=2&AllowTyping=True&ActiveCell='DropdownDashboard'!B2&Item='DropdownDashboard'!A2%3AB22&wdDownloadButton=True&wdInConfigurator=True&wdInConfigurator=True">
                 </iframe>
-                <img className="outline-main rounded-3xl lg:w-3/4 mb-12"
+                <img className="outline-main rounded-3xl lg:w-3/4 mb-12 transform transition duration-300 hover:scale-105"
                             src={prop.mode ? "./visualizations/scatterplot_inverted.png" : "./visualizations/scatterplot.png"}
                             alt="scatterplot"
                         />
-                <div className = " grid grid-cols-2 w-full gap-4 px-4 lg:space-x-6">
+                <div className = "grid grid-cols-2 w-full gap-4 px-4 lg:space-x-6">
                     <div >
-                        <img className="outline-main rounded-3xl"
+                        <img className="outline-main rounded-3xl transform transition duration-300 hover:scale-105"
                             src="./visualizations/highest_cour.png"
                             alt="highest courses"
                         />
                     </div>
                     <div >
-                        <img className="outline-main rounded-3xl"
+                        <img className="outline-main rounded-3xl transform transition duration-300 hover:scale-105"
                             src="./visualizations/lowest_cour.png"
                             alt="lowest courses"
                         />
                     </div>
                     <div >
-                        <img className="outline-main rounded-3xl"
+                        <img className="outline-main rounded-3xl transform transition duration-300 hover:scale-105"
                             src="./visualizations/highest_sub.png"
                             alt="highest subjects"
                         />
                     </div>
                     <div >
-                        <img className="outline-main rounded-3xl"
+                        <img className="outline-main rounded-3xl transform transition duration-300 hover:scale-105"
                             src="./visualizations/lowest_sub.png"
                             alt="lowest subjects"
                         />
                     </div>
                     <div >
-                        <img className="outline-main rounded-3xl"
+                        <img className="outline-main rounded-3xl transform transition duration-300 hover:scale-105"
                             src= {prop.mode ? "./visualizations/highest_vis2_inverted.png" : "./visualizations/highest_vis2.png"}
                             alt="highest courses"
                         />
                     </div>
                     <div >
-                        <img className="outline-main rounded-3xl"
+                        <img className="outline-main rounded-3xl transform transition duration-300 hover:scale-105"
                             src= {prop.mode ? "./visualizations/lowest_vis2_inverted.png" : "./visualizations/lowest_vis2.png"}
                             alt="lowest courses"
                         />
                     </div>
                     <div >
-                        <img className="outline-main rounded-3xl"
+                        <img className="outline-main rounded-3xl transform transition duration-300 hover:scale-105"
                             src={prop.mode ? "./visualizations/Engl_GPA_Inverted.png" : "./visualizations/Engl_GPA.png"}
                             alt="GPA of English Courses Over Time"
                         />
                     </div>
                     <div >
-                        <img className="outline-main rounded-3xl"
+                        <img className="outline-main rounded-3xl transform transition duration-300 hover:scale-105"
                             src= {prop.mode ? "./visualizations/gpatime_inverted.png" : "./visualizations/gpatime.png"}
                             alt="gpa over time"
                         />
                     </div>
                     <div >
-                        <img className="outline-main rounded-3xl"
-                            src= {prop.mode ? "./visualizations/topcalc_inverted.png" : "./visualizations/topcalc.png"}
+                        <img className="outline-main rounded-3xl transform transition duration-300 hover:scale-105"
+                            src= {prop.mode ? "./visualizations/boxplot_inverted.png" : "./visualizations/boxplot.png"}
                             alt="top calc classes"
                         />
                     </div>
                     <div >
-                        <img className="outline-main rounded-3xl"
-                            src= {prop.mode ? "./visualizations/top4calc_inverted.png" : "./visualizations/top4calc.png"}
+                        <img className="outline-main rounded-3xl transform transition duration-300 hover:scale-105"
+                            src= {prop.mode ? "./visualizations/piechart_inverted.png" : "./visualizations/piechart.png"}
                             alt="percent As"
                         />
                     </div>
                     <div >
-                        <img className="outline-main rounded-3xl"
+                        <img className="outline-main rounded-3xl transform transition duration-300 hover:scale-105"
                             src="./visualizations/planecalc2.png"
                             alt="plane calc 2"
                         />
                     </div>
                     <div >
-                        <img className="outline-main rounded-3xl"
+                        <img className="outline-main rounded-3xl transform transition duration-300 hover:scale-105"
                             src="./visualizations/transideas.png"
                             alt="translating ideas"
                         />
                     </div>
                     <div >
-                        <img className="outline-main rounded-3xl"
+                        <img className="outline-main rounded-3xl transform transition duration-300 hover:scale-105"
                             src="./visualizations/planecalc1.png"
                             alt="plane calc 1"
                         />
                     </div>
                     <div >
-                        <img className="outline-main rounded-3xl"
+                        <img className="outline-main rounded-3xl transform transition duration-300 hover:scale-105"
                             src="./visualizations/sciencewriting.png"
                             alt="science & writing"
                         />
                     </div>
                     <div >
-                        <img className="outline-main rounded-3xl"
+                        <img className="outline-main rounded-3xl transform transition duration-300 hover:scale-105"
                             src="./visualizations/appliedcalc2.png"
                             alt="applied calc 2"
                         />
                     </div>
                     <div >
-                        <img className="outline-main rounded-3xl"
+                        <img className="outline-main rounded-3xl transform transition duration-300 hover:scale-105"
                             src="./visualizations/organicchem.png"
                             alt="organic chem"
                         />
@@ -128,7 +128,7 @@ export default function Gallery(prop) {
                 
                     
                 </div>
-                <img className="outline-main rounded-3xl lg:w-1/2 mt-4"
+                <img className="outline-main rounded-3xl lg:w-1/2 mt-4 transform transition duration-300 hover:scale-105"
                             src="./visualizations/range.png"
                             alt="range"
                         />
--- src/components/Paper.js
+++ src/components/Paper.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function Paper() {
     return (
     <section id="Paper">   
-        <div className="container mx-auto flex px-10 py-10 md:flex-row flex-col items-center">
-            <div className="lg:w-full md:w-2/3 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+        <div className="container mx-auto flex px-10 py-10 flex-col">
+            <div className="w-full flex flex-col mb-16 text-center lg:w-3/4 mx-auto">
                 <div className= "dark:bg-gray-800 bg-gray-100 opacity-70 p-4 pb-0 mb-4 rounded-xl w-full text-center">
                     <h1 className="title-font text-5xl mb-4 font-bold text-highlight">
                         Final Group Report Paper
@@ -12,11 +12,9 @@ export default function Paper() {
                 </div>
                 
                 <iframe 
-                title="Final Group Report Paper"
-                src="https://docs.google.com/document/d/e/2PACX-1vS3uqq-4t1A67GZ9wN2rxoigehGO4lKGU7tGULNtXa3s2zPhfhICyPOuGpKFIv2SLXTZzE6_tqk3s1I/pub?embedded=true"
-                width="830"
-                className="mx-auto h-screen"
-                 frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> document, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>  
+                title = "CGT270 - Final Report Paper"
+                src="https://drive.google.com/file/d/1GDshfAGJQEzo0yqq7hfJPMNHHFqWM2qg/preview#view=fith" 
+                width="100%" height="800"/>
             </div>
         </div>
     </section>
